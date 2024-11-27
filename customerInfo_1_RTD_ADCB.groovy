ce6d3b943ad52b6b2e866d6bf593b13a4502c19c
Vyom-Mente <ramanshimbha@gmail.com>
2024-11-27 12:23:47 +0530

    Add files via upload
    RTD for Customer

---
+++ customerInfo_1_RTD_ADCB.groovy
@@ -0,0 +1,329 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import org.apache.camel.Exchange;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+
+
+
+public class RTDRequest {
+	
+private static Map map1 = Collections.synchronizedMap(new HashMap());
+
+public static Map<String, String> fetchCustomerInfo(String EntityID,Map<String, String> map) throws IOException {
+        
+		 Properties props = new Properties();
+        props.load(new FileInputStream("/usr/local/EOP/EOP-1.3.0/config/spring/camel-context.properties"));
+
+        // SSL Configuration
+        String keyPath = "/usr/local/EOP/EOP-1.3.0/config/realtimedemo/rtdkeystore.jks";
+        String keyPass = "changeit";
+        String keyType = "JKS";
+        System.setProperty("javax.net.ssl.keyStore", keyPath);
+        System.setProperty("javax.net.ssl.keyStorePassword", keyPass);
+        System.setProperty("javax.net.ssl.keyStoreType", keyType);
+
+        // OAuth 2.0 Token Retrieval
+        String clientId = props.getProperty("client_id");
+        String clientSecret = props.getProperty("client_secret");
+        String tokenUrl = "https://devmag.adcb.com/auth/oauth/v2/token";
+
+        URL tokenUrlObj = new URL(tokenUrl);
+        HttpsURLConnection tokenConnection = (HttpsURLConnection) tokenUrlObj.openConnection();
+        tokenConnection.setRequestMethod("POST");
+        tokenConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        tokenConnection.setDoOutput(true);
+
+        // Construct the token request body
+        String requestBody = "grant_type=client_credentials&client_id=" + URLEncoder.encode(clientId, "UTF-8") +
+                             "&client_secret=" + URLEncoder.encode(clientSecret, "UTF-8") +
+                             "&scope=Accounts";
+
+        // Send token request
+        try (OutputStream os = tokenConnection.getOutputStream()) {
+            os.write(requestBody.getBytes("utf-8"));
+        }
+
+        // Parse the token response
+        BufferedReader tokenReader = new BufferedReader(new InputStreamReader(tokenConnection.getInputStream()));
+        StringBuilder tokenContent = new StringBuilder();
+        String inputLine;
+        while ((inputLine = tokenReader.readLine()) != null) {
+            tokenContent.append(inputLine);
+        }
+        tokenReader.close();
+
+        // Extract the access token from the response JSON
+        String accessToken = new JSONObject(tokenContent.toString()).getString("access_token");
+        tokenConnection.disconnect();
+
+        if (accessToken == null || accessToken.isEmpty()) {
+            throw new IOException("Failed to obtain access token");
+        }
+      
+	  
+	  
+	  
+	   // Make the API call to fetch customer_info
+	   
+	    String customerId = EntityID;
+        String apiUrl = "https://devmag.adcb.com/v2/customer_info/customerId";
+
+        URL apiURL = new URL(apiUrl);
+        HttpsURLConnection apiConnection = (HttpsURLConnection) apiURL.openConnection();
+        apiConnection.setRequestProperty("Content-Type", "application/json");
+        apiConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
+        apiConnection.setRequestProperty("x-fapi-interaction-id", UUID.randomUUID().toString());
+        apiConnection.setRequestMethod("GET");
+
+        System.out.println("Connection established to " + apiUrl);
+  
+
+        // Reading JSON response
+        InputStreamReader reader = new InputStreamReader(apiConnection.getInputStream());
+        StringBuilder buf = new StringBuilder();
+        char[] cbuf = new char[2048];
+        int num;
+        while (-1 != (num = reader.read(cbuf))) {
+            buf.append(cbuf, 0, num);
+        }
+        String jsonResponse = buf.toString();
+        System.out.println("Response received: " + jsonResponse);
+
+        // Parse JSON response
+        // Parse JSON response
+JSONObject jsonObject = new JSONObject(jsonResponse);
+JSONObject data = jsonObject.optJSONObject("Data").optJSONObject("Customer");
+
+
+
+// Parse EmployerInfo
+JSONObject employerInfo = data.optJSONObject("CustomerPersonalDetails").optJSONObject("EmployerInfo");
+map.put("DESIGNATION", employerInfo.optString("Designation", ""));
+
+
+// Parse PersonalInfo
+JSONObject personalInfo = data.optJSONObject("CustomerPersonalDetails").optJSONObject("PersonalInfo");
+map.put("GENDER", personalInfo.optString("Gender", ""));
+map.put("NON_RESIDENT_FLAG", personalInfo.optString("ResidencyStatus", ""));
+map.put("CUSTOMER_SINCE", personalInfo.optString("RegistrationDate", ""));
+map.put("MOTHER_MAIDEN_NAME", personalInfo.optString("MotherMaidenName", ""));
+map.put("FIRST_NAME", personalInfo.optString("FullName", ""));
+map.put("MIDDLE_NAME", personalInfo.optString("FullName", ""));
+map.put("LAST_NAME", personalInfo.optString("FullName", ""));
+map.put("CUST_RM_NAME", personalInfo.optString("RMName", ""));
+map.put("NATIONALITY", personalInfo.optString("NationalityDesc", ""));
+map.put("MARITIAL_STATUS", personalInfo.optString("MaritalStatus", ""));
+map.put("CUSTOMER_TYPE", personalInfo.optString("Category", ""));
+map.put("ROYAL_FLAG", personalInfo.optString("Category", ""));
+map.put("VIP_FLAG", personalInfo.optString("Category", ""));
+map.put("PEP_FLAG", personalInfo.optString("Category", ""));
+map.put("CUSTOMER_SEGMENT", personalInfo.optString("Category", ""));
+
+
+// Parse KYCInfo
+JSONObject kycInfo = data.optJSONObject("CustomerPersonalDetails").optJSONObject("KYCInfo");
+map.put("PASSPORT_NUMBER", kycInfo.optString("PassportNumber", ""));
+map.put("PASSPORT_EXPIRY_DT", kycInfo.optString("ExpiryDate", ""));
+map.put("VISA_EXPIRY_DT", kycInfo.optString("VisaExpiryDate", ""));
+map.put("EMIRATES_ID_EXPIRY_DT", kycInfo.optString("UAENationalIdExpiryDate", ""));
+map.put("VISA_NUMBER", kycInfo.optString("VisaNumber", ""));
+map.put("TRADE_LICENSE_NUM", kycInfo.optString("TradeLicenseNumber", ""));
+
+
+// Close the connection and return the map
+apiConnection.disconnect();
+return map;
+    }
+
+
+  
+
+
+public void prepareADCBResponse(Exchange exchange) {
+   String textXml = exchange.getIn().getBody();
+		//System.out.println(textXml);
+		String textXml1 = textXml.replace('"','/');
+		String[] splitXml = textXml1.split("entityType=/", 2);
+		String newXml1 = splitXml[1].replaceFirst("/>", "</entityType>");
+		String textXml2 = "<entityRequest><entityType>" + newXml1;
+
+		Document doc = convertStringToXMLDocument(textXml2);
+		Map map = new HashMap();
+
+		String entityType = null;
+		String entityID = null;
+		try 
+			{
+				doc.getDocumentElement().normalize();
+					NodeList nList = doc.getElementsByTagName("entityRequest");
+					Node nNode = nList.item(0);
+					
+					Element eElement = (Element) nNode;
+					entityType = (eElement.getElementsByTagName("entityType")).item(0).getTextContent();
+			        entityID = (eElement.getElementsByTagName("entityID")).item(0).getTextContent();
+						
+			} catch (Exception e) {
+				e.printStackTrace();
+		}
+		
+		switch (entityType)
+			{
+		/*	case "A" : 
+				try {
+					TimeLimitedCodeBlock.runWithTimeout(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								MultifetchAccountInfo m1= new MultifetchAccountInfo();
+								Thread t1= new Thread(m1);
+								
+								t1.start(); 
+								
+
+								t1.join();
+							
+								//mapThread.putAll(map1);
+
+							}
+							catch (InterruptedException e) {
+								System.out.println("was interuupted! 1");
+							}
+						}
+					}, 3, TimeUnit.SECONDS);
+				}
+				catch (TimeoutException e) {
+					System.out.println("Got timeout! 2");
+				}
+				break;    */
+			case "X" : 
+				try {
+					TimeLimitedCodeBlock.runWithTimeout(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								MultifetchCustomerInfo m2= new MultifetchCustomerInfo();
+								Thread t2= new Thread(m2);
+								
+								
+				
+								t2.start(); 
+								t2.join();
+								
+							
+							}
+							catch (InterruptedException e) {
+								System.out.println("was interuupted! 3");
+							}
+						}
+					}, 3, TimeUnit.SECONDS);
+				}
+				catch (TimeoutException e) {
+					System.out.println("Got timeout! 4");
+				}
+				break;
+				
+			/*	case "B" : 
+				try {
+					TimeLimitedCodeBlock.runWithTimeout(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								MultifetchBalanceInfo m3= new MultifetchBalanceInfo();
+								Thread t3= new Thread(m3);
+								
+								
+				
+								t3.start(); 
+								t3.join();
+								
+							
+							}
+							catch (InterruptedException e) {
+								System.out.println("was interuupted! 5");
+							}
+						}
+					}, 3, TimeUnit.SECONDS);
+				}
+				catch (TimeoutException e) {
+					System.out.println("Got timeout! 6");
+				}
+				break;
+		                  */
+}
+map.putAll(map1);
+    
+}
+
+
+
+
+
+
+class MultifetchCustomerInfo implements Runnable{  
+    
+	private String entityID;
+
+	public MultifetchCustomerInfo(String _entityID) {
+		this.entityID = _entityID;
+	}
+	
+    @Override
+	public void run(){  
+		RTDRequest.fetchCustomerInfo(entityID,RTDRequest.map1); 
+	}
+}
+
+
+class TimeLimitedCodeBlock {
+
+	public static void runWithTimeout(final Runnable runnable, long timeout, TimeUnit timeUnit) throws Exception {
+		runWithTimeout(new Callable<Object>() {
+			@Override
+			public Object call() throws Exception {
+				runnable.run();
+				return null;
+			}
+		}, timeout, timeUnit);
+	}
+
+	public static <T> T runWithTimeout(Callable<T> callable, long timeout, TimeUnit timeUnit) throws Exception {
+		final ExecutorService executor = Executors.newSingleThreadExecutor();
+		final Future<T> future = executor.submit(callable);
+		executor.shutdown(); // This does not cancel the already-scheduled task.
+		try {
+			return future.get(timeout, timeUnit);
+		}
+		catch (TimeoutException e) {
+			future.cancel(true);
+			throw e;
+		}
+		catch (ExecutionException e) {
+			//unwrap the root cause
+			Throwable t = e.getCause();
+			if (t instanceof Error) {
+				throw (Error) t;
+			} else if (t instanceof Exception) {
+				throw (Exception) t;
+			} else {
+				throw new IllegalStateException(t);
+			}
+		}
+	}
+
+}
